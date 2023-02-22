08f5b219815f4e5ef06042d87b8627b284656939
ISmoothI <106954634+ISmoothI@users.noreply.github.com>
2023-02-22 13:32:26 -0800

    Update ComplexMulFunc.py

--- ComplexMulFunc.py
+++ ComplexMulFunc.py
@@ -1,15 +1,6 @@
 
-
 #Runs parameters through set formulas and changes EF values
 def ComplexMul(A, B, C, D, E, F):
     E = A * C - B * D
     F = A * D + B * C
     return 1
-
-def main():
-    #Test Values; Remove if going to merge with main
-    ComplexMul(5, 6, 4, 7, 9, 2)
-    
-
-if __name__=='__main__':
-    main()
