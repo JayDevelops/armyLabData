0827fce527b714db7a7cc183902e5429445ea643
Raymond474 <61649291+Raymond474@users.noreply.github.com>
2023-03-08 13:39:20 -0800

    added normal_deviate function

--- DetectionScripts/ADM.py
+++ DetectionScripts/ADM.py
@@ -1,7 +1,13 @@
+<<<<<<< Updated upstream
 import math
 
 import pandas as pd
 
+=======
+import pandas as pd  # Install pandas in python or use Anaconda environment
+import data  # python module including helper functions (In our case, translated Macros from ADM by Jeol)
+import math
+>>>>>>> Stashed changes
 
 # Reads "Data" sheet from ADM by Joel for reference values and returns it
 def read_data():
@@ -44,7 +50,71 @@ def inverse_distance(measure_dist: float, detection_dist: float) -> list:
     # 10 divided by log 10 static, and log of measure distance divided by detection distance log in base 10
     ten_divided_by_log_10, log_m_dist_by_d_dist = (10 / math.log(10, 10)), math.log(measure_dist / detection_dist, 10)
 
+<<<<<<< Updated upstream
     inverse_distances = []
+=======
+        if (s8[i] == 0):
+            s8[i] = -0.001
+        s3[i] = s3[i] + s8[i]
+
+def Ingard():
+    return 2
+
+def ansi_humidity():
+    TOO = 273.15
+    CO = 331.32
+    t = TOO + t1 #T1 is Temp deg C from excel.
+    PSTAR = 1
+    acr1 = 0.000000016 * (1.377 * t / (t + 110.4)) / PSTAR
+    thto = 2239.1 / t
+    exthto = math.exp(-thto)
+    rto = thto / (1 - exthto)
+    mumaxo = 0.20948 * 4 * math.pi * rto * rto * exthto / 35
+    thtn = 3352 / t
+    exthtn = math.exp(-thtn)
+    rtn = thtn / (1 - exthtn)
+    mumaxn = 0.78084 * 4 * math.pi * rtn * rtn * exthtn / 35
+    cs = CO * math.sqrt(t / TOO)
+    h = h1 * math.exp(math.log10 * (20.5318 - 2939 / t - 2.13759744 * math.log(t))) / PSTAR #H1 is RH% from excel
+    fox = (24 + 44100 * h * (0.05 + h) / (0.391 + h)) * math.sqrt(293 / t)    # Is 293 used as 273 + 20degC?
+    fni = (9 + 350 * h) * (293 / t)
+    d1 = r * 0.01 #R can be a different value depending on what helper function is called
+
+    for i in range(24):
+        freq2 = s4[i] #S4 is from the Freq. Hz collumn
+        freq2 = freq2 * freq2
+        acr = acr1 * freq2
+        x2o = freq2 / (fox * fox)
+        avibo = 868.5 * mumaxo * fox / cs * x2o / (1 + x2o)
+        x2n = freq2 / (fni * fni)
+        avibn = 868.5 * mumaxn * fni / cs * x2n / (1 + x2n)
+        atm_abs[i] = -d1 * (acr + avibo + avibn) #AtmAbs, atmosphere absorption, a global variable this is where it is initialized
+
+
+def inverse_distance(s8, s3, d3, d4):
+    TEN_DIV_NATURAL_LOG10= (10 / math.log(10, 10))
+    #D4 should be mic distance source m. changes depending on what macro is used. Can also be detect distance
+    p_inv = 2 * TEN_DIV_NATURAL_LOG10 * math.log(d3 / d4) #Inverse distance loss from D3 to D4. D3 is the mic distance from the target from excel.
+
+    for i in range(24):
+        s8[i] = p_inv
+        if (s8[i] == 0):
+            s8[i] = -0.001
+        s3[i] = s8[i]
+
+def normal_deviate(p):
+    t = math.sqrt(-2 * math.log(p))
+    return t - (2.30753 + t * 0.27061) / (1 + t * (0.99229 + t * 0.04481))
+
+
+"""
+ground_effect_reference(26) is 'Reference Ground Effect during measurement' from .vbs file
+"""
+def reference_calc(ground_effect_ref):
+    for I in range(0,23):
+        ground_effect_ref(I) = ground_effect(I)
+        AtmAbsRef(I) = atmos_absorption(I)
+>>>>>>> Stashed changes
 
     for x in range(0, 23):
         inv_dist = 2 * ten_divided_by_log_10 * log_m_dist_by_d_dist
