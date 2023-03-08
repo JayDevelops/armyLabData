01b21a1f0206ca98f7b6e728497f99972faabf14
p-han3 <46765867+p-han3@users.noreply.github.com>
2023-03-08 13:15:00 -0800

    Update ADM.py
    Moved data.py methods into the adm.py for easier variable passing.

--- DetectionScripts/ADM.py
+++ DetectionScripts/ADM.py
@@ -2,18 +2,31 @@ import pandas as pd #Install pandas in python or use Anaconda environment
 import data #python module including helper functions (In our case, translated Macros from ADM by Jeol)
 
 def Detection():
-    data_df = data.read_data()
-    print(data_df.iloc[:,29:34])
-
+    # data_df = data.read_data()
+    # print(data_df.iloc[:,29:34])
+    
+    data_df = pd.read_excel('ADM - from Joel - Sept-2013.xls', sheet_name='Data')
+    
     #Instantiating specific columns to be pulled from the data sheet in ADM by Joel
     Trg_name = 'Drone' #Drone db column 
     Bkg_name = 'Urban' #Ambient setting background noise
     Hth_name = 'ISO Std' #Hearing Threshold based on ISO Standard
 
-    #Find and set all necessary dataframes for further processing
-    freq_df, target_df, bkg_noise_df, \
-        hear_thresh_df, awt_weights, ai_weights_df, \
-        third_obands_df = data.get_data(Trg_name, Bkg_name, Hth_name, data_df)
+    freq_df = data_df['Freq Hz']
+    target_df = data_df[Trg_name]
+    bkg_df = data_df[Bkg_name]
+    ht_df = data_df[Hth_name]
+    awt_weights_df = data_df['Awt weights']
+    ai_weights_df = data_df['A.I. weights']
+    third_octave_bands_df = data_df.iloc[:,29:34].copy()
+
+    # #Find and set all necessary dataframes for further processing
+    # freq_df, target_df, bkg_noise_df, \
+    #     hear_thresh_df, awt_weights, ai_weights_df, \
+    #     third_obands_df = data.get_data(Trg_name, Bkg_name, Hth_name, data_df)
+    
+    # print(third_obands_df)
+    
 
 #Main Function Declaration and Call
 def main():
